4d41dfb3ec70e579fda59b74921fac10ad354ede
cjwalle97 <cjwalle97@gmail.com>
2017-01-06 15:00:21 -0600

    Update
    Update

--- ConditionalsCSharp/Program.cs
+++ ConditionalsCSharp/Program.cs
@@ -12,11 +12,11 @@ namespace ConditionalsCSharp
         int ReturnLarger(int a, int b)
         {
             int Largest = 0;
-            if(a>b)
+            if (a > b)
             {
                 Largest = a;
             }
-            if(a<b)
+            if (a < b)
             {
                 Largest = b;
             }
@@ -24,14 +24,79 @@ namespace ConditionalsCSharp
         }
 
         //3. 
-        int order(int a, int b, int c, int d, int e)
+        void order(int a, int b, int c, int d, int e)
         {
-            int total = a + b + c + d + e;
-            if(total > 0)
+            int Highest = 0;
+            int High = 0;
+            int Middle = 0;
+            int Low = 0;
+            int Lowest = 0;
+            int temp = 0;
+            List<int> Numbers = new List<int>();
+            Numbers.Add(a);
+            Numbers.Add(b);
+            Numbers.Add(c);
+            Numbers.Add(d);
+            Numbers.Add(e);
+            for (int i = 0; i < 5; i++)
             {
-
+                if (Numbers[i] > Highest)
+                {
+                    temp = Highest;
+                    Highest = Numbers[i];
+                    High = temp;
+                }
+                if (Numbers[i] > High && Numbers[i] < Highest)
+                {
+                    temp = High;
+                    High = Numbers[i];
+                    Middle = temp;
+                }
+                if (Numbers[i] > Middle && Numbers[i] < High)
+                {
+                    temp = Middle;
+                    Middle = Numbers[i];
+                    Low = temp;
+                }
+                if (Numbers[i] > Low && Numbers[i] < Middle)
+                {
+                    temp = Low;
+                    Low = Numbers[i];
+                    Lowest = temp;
+                }
+                else
+                {
+                    Lowest = Numbers[i];
+                }
+            }
+            int total = a + b + c + d + e;
+            if (total > 0)
+            {
+                Console.WriteLine(Lowest);
+                Console.WriteLine(Low);
+                Console.WriteLine(Middle);
+                Console.WriteLine(High);
+                Console.WriteLine(Highest);
+            }
+            if (total < 0)
+            {
+                Console.WriteLine(Highest);
+                Console.WriteLine(High);
+                Console.WriteLine(Middle);
+                Console.WriteLine(Low);
+                Console.WriteLine(Lowest);
+            }
+            if (total == 0)
+            {
+                Console.WriteLine(a);
+                Console.WriteLine(b);
+                Console.WriteLine(c);
+                Console.WriteLine(d);
+                Console.WriteLine(e);
             }
         }
+
+
         static void Main(string[] args)
         {
             //1.
@@ -42,6 +107,80 @@ namespace ConditionalsCSharp
                 x = 100;
             }
 
+            //4.
+            int choice = 2;
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine(1);
+                    break;
+                case 2:
+                    Console.WriteLine(2);
+                    break;
+                case 3:
+                    Console.WriteLine(3);
+                    break;
+                case 4:
+                    Console.WriteLine(4);
+                    break;
+            }
+
+            //5.
+            y = (x == 0) ? 0 : 10 / x;
+
+            //6.
+            int a = 5;
+            int b = 2;
+            int result = 0;
+            char sign = '+';
+            switch (sign)
+            {
+                case '+':
+                    result = a + b;
+                    break;
+
+                case '-':
+                    result = a - b;
+                    break;
+
+                case '*':
+                    result = a * b;
+                    break;
+
+                case '/':
+                    result = a / b;
+                    break;
+
+                case '%':
+                    result = a % b;
+                    break;
+            }
+
+            //7.
+            int month = 2;
+            switch (month)
+            {
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+                    Console.WriteLine(31);
+                    break;
+                case 2:
+                    Console.WriteLine(28);
+                    break;
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    Console.WriteLine(30);
+                    break;                  
+            }
+            //8.
+
+        }
         }
     }
-}
